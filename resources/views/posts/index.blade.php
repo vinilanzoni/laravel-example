65684d5aad5c605a0daf230f25c8a9c7dba56ad2
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2017-08-11 19:08:02 -0300

    Begin of project with some modifications

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.master')
+
+@section ('content')
+  <div class="blog-post">
+    <h2 class="blog-post-title">Sample blog post</h2>
+    <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+    <p>One blog post</p>
+  </div><!-- /.blog-post -->
+ @endsection
